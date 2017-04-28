82b8cffcb915053675f2866618f25f4a45ffe4d0
Marlito Dungog <marlito.dungog@gmail.com>
2017-04-28 16:21:13 -0700

    Updated slider testimonial    Added slider nav

--- wp-content/themes/custom-theme/footer-inner.php
+++ wp-content/themes/custom-theme/footer-inner.php
@@ -78,8 +78,7 @@ $(document).ready(function(){
       items: 1,
       loop:true,
       autoplay: true,
-      margin:10,
-      nav:true,
+      margin:10     
 
   });
   $(".gallery-block").hover(function() {
